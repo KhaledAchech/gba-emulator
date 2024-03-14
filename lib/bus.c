5fa31a90edb9513fcfe1100b16999a24f7fda5de
KhaledAchech <achechkhaled@gmail.com>
2024-03-14 11:56:19 +0100

    cpu proc instructions

--- lib/bus.c
+++ lib/bus.c
@@ -1,15 +1,21 @@
 #include <bus.h>
+#include <cart.h>
 
 u8 bus_read(u16 address) {
-    // ROM Data.
-    if (address < 0x8000) return cart_read(address);
+    if (address < 0x8000) {
+        // ROM Data.
+        return cart_read(address);
+    }
     
     NO_IMPL
 }
 
 void bus_write(u16 address, u8 value){
-    // ROM Data.
-    if (address < 0x8000) cart_write(address, value);
+    if (address < 0x8000)  {
+        // ROM Data.
+        cart_write(address, value);
+        return;
+    }
     
     NO_IMPL
 }
